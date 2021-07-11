b41138aee1ed8fa08d2c25fae855f635339def63
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-07-11 17:51:07 +0530

    Fixed the design of the top-section of chat drawer

--- client/src/components/Room/Chat.js
+++ client/src/components/Room/Chat.js
@@ -2,25 +2,27 @@ import React from 'react';
 import '../../styles/Chat.scss';
 import { ChatDrawer } from '../MaterialComponents';
 import { IconButton } from '@material-ui/core';
-import { ArrowForwardIos } from '@material-ui/icons'
+import { Close } from '@material-ui/icons'
 
 function Chat({ open, setChatOpen }) {
   return (
     <>
       <ChatDrawer open={open} variant="persistent" anchor="right">
         <div id="chat-drawer-body">
-          <div className="drawer-head-area">
-            <div className="drawer-header">
-              <h1 className="header">AirConf</h1>
+          <div className="head-area">
+            <div className="header-inner-area">
+              <div className="header-text-area">
+                <h1 className="header">In-call messages</h1>
+              </div>
             </div>
             <IconButton onClick={() => setChatOpen(false)}>
-              <ArrowForwardIos fontSize="small" style={{color: '#64379f'}} />
+              <Close fontSize="small" style={{color: '#64379f'}} />
             </IconButton>
           </div>
-          <div className="drawer-chat-area">
+          <div className="chat-area">
 
           </div>
-          <div className="drawer-bottom-area">
+          <div className="bottom-area">
 
           </div>
         </div>
