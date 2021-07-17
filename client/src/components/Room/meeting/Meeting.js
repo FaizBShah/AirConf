d595883b1bd93497aa877c6584a1ced21d8a9a31
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-07-17 14:04:39 +0530

    Made the top part of Meetings area and implemented the copy url function

--- client/src/components/Room/meeting/Meeting.js
+++ client/src/components/Room/meeting/Meeting.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../styles/Meeting.scss';
 import { useWindowDimensions } from '../../../utils/windowUtils';
 import { getChatWidth } from '../../../utils/getChatWidth';
+import { Tooltip, IconButton } from '@material-ui/core'
+import { Share } from '@material-ui/icons';
 
 function Meeting({ chatOpen }) {
+  const [roomId, setRoomId] = useState("");
   const { width } = useWindowDimensions();
+
+  useEffect(() => {
+    setRoomId(getRoomId(window.location.pathname));
+  }, []);
+
+  const getRoomId = (path) => {
+    return path.split('/')[2];
+  }
+
+  const copyRoomId = () => {
+    const el = document.createElement('textarea');
+    el.value = window.location.href;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
 
   return (
     <>
       <div id="meeting-area" style={{width: chatOpen ? `calc(100% - ${getChatWidth(width)})` : '100%'}}>
         <div className="meeting-info">
-
+          <Tooltip title="Share Link">
+            <IconButton onClick={copyRoomId}>
+              <Share style={{color: '#ddacf5'}} />
+            </IconButton>
+          </Tooltip>
+          <div className="room-id-area">
+            <h3 className="room-id">{roomId}</h3>
+          </div>
         </div>
         <div className="videos-area">
 
