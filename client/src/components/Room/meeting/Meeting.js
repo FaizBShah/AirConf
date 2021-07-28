104ad3be36eccd0876d615d6d0ac71c237dfa3ae
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-07-28 15:41:42 +0530

    Completed the Videos Page UI

--- client/src/components/Room/meeting/Meeting.js
+++ client/src/components/Room/meeting/Meeting.js
@@ -68,7 +68,7 @@ function Meeting({ chatOpen }) {
                 md={6}
                 xs={12}
               >
-                <Video video={video} />
+                <Video stream="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4" />
               </Grid>
             ))}
           </Grid>
--- client/src/components/Room/meeting/Video.js
+++ client/src/components/Room/meeting/Video.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../styles/Video.scss';
+import { Tooltip, IconButton } from '@material-ui/core';
+import { MicOff, AccountCircle } from '@material-ui/icons';
 
-function Video() {
+function Video({ stream }) {
+  const [isVideoOn, setIsVideoOn] = useState(false);
+
   return (
     <>
-      <div className="video-card">
-        <video className="video-stream" src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4">
-          Sorry, there was an error in displaying the stream
-        </video>
+      <div className="video-card" style={{minHeight: !isVideoOn ? '14rem' : 'auto'}}>
+        {isVideoOn && (
+          <video className="video-stream" src={stream}>
+            Sorry, there was an error in displaying the stream
+          </video>
+        )}
+        <div className="video-info" style={{background: !isVideoOn ? '#64379f' : 'transparent'}}>
+          <div className="info-header">
+            <Tooltip title="Muted">
+              <IconButton style={{background: 'rgba(0, 0, 0, 0.3)', margin: '0.2rem', height: '2rem', width: '2rem'}}>
+                <MicOff style={{color: 'white', fontSize: '14px'}} />
+              </IconButton>
+            </Tooltip>
+          </div>
+          {!isVideoOn && (
+            <>
+              <div className="user-icon">
+                <AccountCircle style={{color: '#ddacf5', fontSize: '6rem'}} />
+              </div>
+              <div className="user-name-area">
+                <h4 className="username">Faiz Shah</h4>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </>
   )
